d855d245ec9a14c752e4dfc6a7cc8f401562b975
ZadayNava <sadaynava98@gmail.com>
2024-12-09 14:43:47 -0600

    DAL - Camiones(Read)

---
+++ DAL-CapaAccesoDatos/DAL_Camiones.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VO;
+
+namespace DAL_CapaAccesoDatos
+{
+    internal class DAL_Camiones
+    {
+        //Create
+
+        //Read
+        public static List<Camiones_VO> GetCamiones(params object[] parametros)
+        {
+            //creo una lista de objetos vo
+            List<Camiones_VO> list = new List<Camiones_VO>();
+            try
+            {
+                //creo un dataSet el cual recibira lo que devuelva la ejecucion del metodo "execute_DataSet" de la clase "metodos_datos"
+                DataSet ds_camiones = Metodos_Datos.execute_DataSet("Sp_Listar_camiones", parametros);
+                //recorro cada renglon existente de nuestro ds creando obetos del tipo VO y añadiendolos a la lista
+                foreach (DataRow dr in ds_camiones.Tables[0].Rows)
+                {
+                    list.Add(new Camiones_VO(dr));
+                }
+                return list;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            //Update
+
+            //Delete
+        }
+    }
+}
